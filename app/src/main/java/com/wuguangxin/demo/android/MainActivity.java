b4c6a0267254e0b3da066184480eb2eab4186d74
wuguangxin <1585252390@qq.con>
2017-06-14 16:46:57 +0800

    Initial commit

---
+++ app/src/main/java/com/wuguangxin/demo/android/MainActivity.java
@@ -0,0 +1,27 @@
+package com.wuguangxin.demo.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wuguangxin.dialog.MyDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
+        mDialog = new MyDialog(this);
+        mDialog.setTitle("提示");
+        mDialog.setMessage("欢迎使用XinAndroid");
+        mDialog.setPositiveButton("确认", null);
+        mDialog.setNegativeButton("取消", null);
+        mDialog.show();
+    }
+}
